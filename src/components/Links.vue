dde3f1204b2f16c91dbfd60f5e5b359b544fd243
shiqustudio <794704796@qq.com>
2024-01-30 16:48:18 +0800

    update🎉

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Airbnb,Brain,BattleNet,Chrome,Bookmark,Tools } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,Airbnb,Brain,BattleNet,Chrome,Bookmark,Tools,KiwiBird,Crow,Sellsy } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -76,6 +76,9 @@ const siteIcon = {
   Chrome,
   Bookmark,
   Tools,
+  KiwiBird,
+  Crow,
+  Sellsy,
 };
 
 // 链接跳转
